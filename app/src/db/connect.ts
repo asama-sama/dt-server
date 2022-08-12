07e6da4cf8d58d9fbec860ddb69d2b5a0ed53a58
Asama Qureshi <asama.qureshi@gmail.com>
2022-08-12 13:08:10 +1000

    connection to database

---
+++ app/src/db/connect.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize-typescript";
+
+export const getConnection = async () => {
+  const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+  if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST) {
+    throw new Error("DB connection details must be provided");
+  }
+
+  const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "postgres",
+    models: [`${__dirname}/models`],
+  });
+
+  try {
+    await connection.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+  return connection;
+};
--- app/src/index.ts
+++ app/src/index.ts
@@ -2,8 +2,11 @@ import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import { getConnection } from "./db/connect";
 
 dotenv.config();
+
+console.log("test", process.env);
 
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
@@ -11,6 +14,8 @@ const app: Express = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = getConnection();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
