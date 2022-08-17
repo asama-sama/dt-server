49df03ab40266e658d54e220aa9a0ccc1980a036
Asama Qureshi <asama.qureshi@gmail.com>
2022-08-17 11:25:05 +1000

    added logs on nomatim to see when fetches are happening

--- app/src/clients/nominatim.ts
+++ app/src/clients/nominatim.ts
@@ -12,11 +12,13 @@ export const bulkSearch = async (queries: string[]) => {
     return [];
   }
   for (const query of queries) {
+    console.log(`fetch ${query} from nomatim`);
     const res = await axios.get(
       `https://nominatim.openstreetmap.org/search?q=${query}&format=json&polygon_geojson=1&addressdetails=1&countrycodes=au&limit=1`
     );
     results.push(res.data[0]);
     await new Promise((r) => setTimeout(r, apiTimeout)); // wait 1.5 seconds to not make nominatim angry
   }
+  console.log("nomatim fetch complete");
   return results;
 };
