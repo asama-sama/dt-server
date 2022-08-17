491abddfdaa3b654ef2e5351229ce8f8a73eae0d
Asama Qureshi <asama.qureshi@gmail.com>
2022-08-17 10:16:29 +1000

    pushing correct data to suburb geodata

--- app/src/clients/nominatim.ts
+++ app/src/clients/nominatim.ts
@@ -15,7 +15,7 @@ export const bulkSearch = async (queries: string[]) => {
     const res = await axios.get(
       `https://nominatim.openstreetmap.org/search?q=${query}&format=json&polygon_geojson=1&addressdetails=1&countrycodes=au&limit=1`
     );
-    results.push(res);
+    results.push(res.data[0]);
     await new Promise((r) => setTimeout(r, apiTimeout)); // wait 1.5 seconds to not make nominatim angry
   }
   return results;
