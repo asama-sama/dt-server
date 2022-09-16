7d885e20edaedc25536280ed4fd0c8efb4faf5a3
Asama Qureshi <asama.qureshi@gmail.com>
2022-09-16 11:07:06 +1000

    remove unused routes

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,16 +1,12 @@
 import express from "express";
-import { emissions } from "./emissions";
 import { suburbs } from "./suburbs";
-import { categories } from "./categories";
 import { apis } from "./apis";
 import { airQualityRoutes } from "./airQuality";
 import { trafficVolumeRoutes } from "./trafficVolume";
 
 const router = express.Router();
 
-router.use("/emissions", emissions);
 router.use("/suburbs", suburbs);
-router.use("/categories", categories);
 router.use("/apis", apis);
 router.use("/airquality", airQualityRoutes);
 router.use("/trafficvolume", trafficVolumeRoutes);
