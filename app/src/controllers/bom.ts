60d8be5d27d1d50354b9230d282c80c51110def5
Asama Qureshi <asama.qureshi@gmail.com>
2022-11-28 16:22:03 +1100

    change hourly to daily air quality types. moved transaction inside each fetch for update bom stations

--- app/src/controllers/bom.ts
+++ app/src/controllers/bom.ts
@@ -53,9 +53,9 @@ export const updateReadings = async () => {
   const loader = new Loader(stations.length, "BOM Readings");
   const connection = getConnection();
   for (const station of stations) {
-    await connection.transaction(async (trx) => {
-      await getStationWeather(station)
-        .then(async (observations) => {
+    await getStationWeather(station)
+      .then(async (observations) => {
+        await connection.transaction(async (trx) => {
           for (const observation of observations) {
             const { lat, lon } = observation;
             if (station.position === null && (lat || lon)) {
@@ -103,19 +103,19 @@ export const updateReadings = async () => {
               transaction: trx,
             });
           }
-        })
-        .catch(async (e) => {
-          let message = `error loading weather station ${station.name}`;
-          if (e instanceof Error) {
-            message += `: ${e.message}`;
-          }
-          await DataSourceUpdateLog.create({
-            dataSourceId: dataSource?.id,
-            status: UpdateStatus.FAIL,
-            message,
-          });
         });
-      loader.tick();
-    });
+      })
+      .catch(async (e) => {
+        let message = `error loading weather station ${station.name}`;
+        if (e instanceof Error) {
+          message += `: ${e.message}`;
+        }
+        await DataSourceUpdateLog.create({
+          dataSourceId: dataSource?.id,
+          status: UpdateStatus.FAIL,
+          message,
+        });
+      });
+    loader.tick();
   }
 };
